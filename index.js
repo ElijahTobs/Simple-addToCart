cbd803af035a808dfd6b567a850b4347e815a4ff
Comfort Ige <eligenius@gmail.com>
2023-05-28 22:53:25 +0100

    Import functions from firebase

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
+const appSettings = {
+  databaseURl: "https://realtime-database-97858-default-rtdb.firebaseio.com/"
+}
+
 
 
 addButtonEl.addEventListener("click", function() {
