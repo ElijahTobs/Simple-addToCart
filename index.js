c1460f53a6d8c0057e3006a54d8f8840e6bc60a6
Comfort Ige <eligenius@gmail.com>
2023-05-28 20:41:16 +0100

    Display shopping list items on page

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
-  console.log(inputValue)
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
+
