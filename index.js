f7e0a628f173f81862c36328b050c07dd9e1df8d
Comfort Ige <eligenius@gmail.com>
2023-05-28 23:58:22 +0100

    Populate firebase with input field values

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 const appSettings = {
-  databaseURl: "https://realtime-database-97858-default-rtdb.firebaseio.com/"
+  databaseURl: "https://realtime-sample-2-default-rtdb.firebaseio.com/"
 }
 
-
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
-
+  
+  push(shoppingListInDB, inputValue)
+  
   clearInputFieldEl()
+
   appendToShoppingListEl(inputValue)
 })
 
