f2cf8527d926c56754a2ee79fba6314e320abfd6
Comfort Ige <eligenius@gmail.com>
2023-05-29 22:45:52 +0100

    Display database items in shoppingListEl

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
 const appSettings = {
-  databaseURl: "https://realtime-sample-2-default-rtdb.firebaseio.com/"
+  databaseURL: "https://realtime-sample-2-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
@@ -33,3 +30,16 @@ function clearInputFieldEl(){
 function appendToShoppingListEl(value){
   shoppingListEl.innerHTML += `<li>${value}</li>`
 }
+
+onValue(shoppingListInDB, function(snapshot){
+  // console.log(snapshot.val())
+  let shoppingListArray = Object.values(snapshot.val())
+
+  // clearInputFieldEl()
+
+  for (let x in shoppingListArray){
+    let currentValue = shoppingListArray[x]
+    appendToShoppingListEl(currentValue)
+
+  }
+})
