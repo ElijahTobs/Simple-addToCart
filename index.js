12c5a345eb51501a0b3fc7ed3d3c64ed382a9cc1
Comfort Ige <eligenius@gmail.com>
2023-05-30 12:10:17 +0100

    Add event listener to remove item from database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://realtime-sample-2-default-rtdb.firebaseio.com/"
@@ -15,24 +15,13 @@ const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
-  
   push(shoppingListInDB, inputValue)
   
   clearInputFieldEl()
-
-  // appendToShoppingListEl(inputValue)
 })
 
 function clearInputFieldEl(){
   inputFieldEl.value = ""
-}
-
-function appendToShoppingListEl(value){
-  const newEl = document.createElement("li")
-  newEl.textContent = `${value}`
-
-  shoppingListEl.append(newEl)
-  // shoppingListEl.innerHTML += `<li>${value}</li>`
 }
 
 function clearShoppingListEl(){
@@ -41,16 +30,29 @@ function clearShoppingListEl(){
 
 onValue(shoppingListInDB, function(snapshot){
   let shoppingListArray = Object.entries(snapshot.val())
-
   clearShoppingListEl()
 
   for (let x in shoppingListArray){
     let currentItem = shoppingListArray[x]
-    let currentItemID = currentItem[0]
-    let currentItemValue = currentItem[1]
+    // let currentItemID = currentItem[0]
+    // let currentItemValue = currentItem[1]
 
-
-    appendToShoppingListEl(currentItemValue)
-
+    appendToShoppingListEl(currentItem)
   }
 })
+
+function appendToShoppingListEl(itemArr){
+  let itemID = itemArr[0]
+  let itemValue = itemArr[1]
+
+  const newEl = document.createElement("li")
+  newEl.textContent = itemValue
+
+  newEl.addEventListener("click", ()=>{
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+    remove(exactLocationOfItemInDB)
+  })
+
+  shoppingListEl.append(newEl)
+  // shoppingListEl.innerHTML += `<li>${value}</li>`
+}
